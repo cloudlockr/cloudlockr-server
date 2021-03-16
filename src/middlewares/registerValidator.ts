0f98619f4b72197e6c434dce81ab6b8d4f378306
Daniel Ng <danielnsyu@protonmail.com>
2021-03-15 19:02:55 -0700

    added confirmation password field requirement for register

--- src/middlewares/registerValidator.ts
+++ src/middlewares/registerValidator.ts
@@ -9,6 +9,15 @@ const emailUsed: CustomValidator = async (email) => {
   return Promise.resolve();
 };
 
+const samePassword: CustomValidator = async (password1, { req }) => {
+  const password = req.headers?.password;
+
+  if (password === password1) {
+    return Promise.resolve();
+  }
+  return Promise.reject("Passwords don't match");
+};
+
 const registerValidator = (): Array<ValidationChain> => {
   return [
     header("email")
@@ -19,6 +28,7 @@ const registerValidator = (): Array<ValidationChain> => {
     header("password")
       .isLength({ min: 10 })
       .withMessage("Password must be at least 10 characters long"),
+    header("password1").custom(samePassword),
   ];
 };
 
